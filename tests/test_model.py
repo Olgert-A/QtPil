b48db6e58341fa7c65eb75dec93689797d2f08a1
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-17 00:07:01 +0500

    method name refactor

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,6 +3,6 @@ from model import Model
 
 def test_model():
     model = Model()
-    [model.add(f'.\\{i}.bmp') for i in range(1, 6)]
+    [model.add_image(f'.\\{i}.bmp') for i in range(1, 6)]
     assert model.images['1.bmp']
     assert model.folder == '.\\'
