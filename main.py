09b60dcb297920f983ad5e405dfe798d3a5c3d96
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-16 16:54:11 +0500

    view
    -add window init method (init widgets, layout, connect signals)
    -add load image button
    -place load image example in button signal

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QPushButton
 from PyQt6.QtGui import QPixmap
 
 from PIL import Image
-from PIL.ImageQt import ImageQt, QImage
+from PIL.ImageQt import ImageQt
 
 
 class MainWnd(QMainWindow):
     def __init__(self):
         super().__init__()
-        label = QLabel()
-        image = Image.open('.\\3.bmp')
+        self.widgets = {}
+
+        self.init_window()
+
+    def image_open_clicked(self):
+        self.set_image(self.to_pixmap(Image.open('.\\3.bmp')))
+
+    def init_window(self):
+        img = self.widgets['image'] = QLabel()
+        btn = self.widgets['load'] = QPushButton('Load next')
+        btn.pressed.connect(self.image_open_clicked)
+
+        layout = QVBoxLayout()
+        layout.addWidget(btn)
+        layout.addWidget(img)
+
+        main_widget = QWidget()
+        main_widget.setLayout(layout)
+        self.setCentralWidget(main_widget)
+
+    def set_image(self, image: QPixmap):
+        if image:
+            self.widgets['image'].setPixmap(image)
+
+    @staticmethod
+    def to_pixmap(image: Image) -> QPixmap:
         img1 = ImageQt(image)
         img2 = img1.copy()
-        pix = QPixmap.fromImage(img2)
-        label.setPixmap(pix)
-        self.setCentralWidget(label)
+        return QPixmap.fromImage(img2)
 
 
 app = QApplication(sys.argv)
