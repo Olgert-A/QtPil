843a1fea61316e754512ad853bda401e57f0f41c
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-16 16:38:12 +0500

    create data model

---
+++ model.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+
+class Model:
+    folder = ''            # contain path to image files without name. Same to all files
+    data = {}              # key = name of image with extension, value = pil image object
+
+    def add(self, path):
+        if path:
+            name, self.folder = self._divide_path(path)
+            image = Image.open(path)
+            if image:
+                self.data[name] = image
+
+    @staticmethod
+    def _divide_path(path):
+        name = path.split('\\')[-1]
+        folder = path.replace(name, '')
+        return name, folder
