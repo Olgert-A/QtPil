90c7c1478ee08cfb32ea79de646a73edb1d8f4b3
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-18 19:45:47 +0500

    edit model impl to fit contract

--- model/impl.py
+++ model/impl.py
@@ -1,4 +1,6 @@
-from PIL import Image, ImageDraw
+from os.path import normpath
+from PIL import Image
+from model.contract import ModelContract
 
 
 class Coord:
@@ -22,21 +24,12 @@ class OverlayImage:
         self.transparency = 100
 
 
-class ModelImpl:
+class ModelImpl(ModelContract):
     def __init__(self):
         self.folder = ''            # contain path to image files without name. Same to all files
         self.images = {}            # key = name of image with extension, value = pil image object
-
         self.current_image_name = ''
-
         self.overlay = None
-
-    def add_image(self, path):
-        if path:
-            name, self.folder = self._divide_path(path)
-            image = Image.open(path)
-            if image:
-                self.images[name] = ImageItem(image, None)
 
     def load_overlay_image(self, path):
         if path:
@@ -44,15 +37,28 @@ class ModelImpl:
             if image:
                 self.overlay = OverlayImage(image)
 
-    def clear(self):
+    def close_overlay_image(self):
+        if self.overlay and (image := self.overlay.image):
+            image.close()
+
+    def load_image_list(self, paths):
+        for path in paths:
+            path = normpath(path)
+            name, self.folder = self._divide_path(path)
+            image = Image.open(path)
+            if image:
+                self.images[name] = ImageItem(image, None)
+
+    def get_image_list(self, only_names=True):
+        return [name if only_names else self.folder+name
+                for name in self.images.keys()]
+
+    def clear_image_list(self):
         for _, item in self.images.items():
             item.image.close()
 
         self.images.clear()
         self.folder = ''
-
-    def get_image_list(self):
-        return self.images.keys()
 
     def set_current_image(self, name):
         if name and name in self.images.keys():
@@ -62,9 +68,12 @@ class ModelImpl:
         if self.current_image_name:
             self.images[self.current_image_name].coord = Coord(x, y)
 
-    def get_current_blend(self):
+    def get_current_blended(self):
         if self.current_image_name:
             return self._blend_image(self.current_image_name)
+
+    def save_all(self, path=''):
+        pass
 
     def _blend_image(self, name):
         if name in self.images.keys():
